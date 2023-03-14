0e95f5b6dc0e6e8000879a25f2a82d574b0682e4
Linuxe <angelren1220@gmail.com>
2023-03-14 13:18:01 -0400

    spinner1.js

--- spinner1.js
+++ spinner1.js
@@ -1,27 +1,21 @@
-for (let i = 0; i < 2; i++) {
-  setTimeout(() => {
-    process.stdout.write('\r|   ');
-  }, 100 + i * 700);
-  
-  setTimeout(() => {
-    process.stdout.write('\r/   ');
-  }, 300 + i * 700);
-  
-  setTimeout(() => {
-    process.stdout.write('\r-   ');
-  }, 500 + i * 700);
-  
-  setTimeout(() => {
-    process.stdout.write('\r\\   '); 
-  }, 700 + i * 700);  
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
 
-}
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 700);
 
 setTimeout(() => {
   process.stdout.write('\r|   ');
-}, 100 + 1500);
+}, 900);
 
 process.stdout.write('hello from spinner1.js... \rheyyy\n');
-setTimeout(() => {
-  console.log("\n");
-}, 1700);
